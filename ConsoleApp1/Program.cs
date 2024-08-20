623578e97e54180014aad09635d89e523572afbd
YSK <soonkeat.yeoh33@gmail.com>
2024-08-20 11:36:41 +0700

    feat: console input notification channel parser

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        String? input;
+        do
+        {
+            Console.WriteLine("Notification title Input:");
+            input = Console.ReadLine();
+            if (input != null)
+            {
+                Console.WriteLine(ParseNotificationChannels(input.Trim()));
+            }
+        } while (input == null);
+    }
+
+    static string ParseNotificationChannels(string title)
+    {
+        List<string> validChannels = new List<string> { "BE", "FE", "QA", "Urgent" };
+        HashSet<string> foundChannels = new HashSet<string>();
+
+        // Regex to detect differnet channels from string
+        Regex tagPattern = new Regex(@"\[(.*?)\]");
+        MatchCollection matches = tagPattern.Matches(title);
+
+        // Loop matches and check if they are valid channels and check if channel already listed
+        foreach (Match match in matches)
+        {
+            string tag = match.Groups[1].Value;
+            if (validChannels.Contains(tag) && !foundChannels.Contains(tag))
+            {
+                foundChannels.Add(tag);
+            }
+        }
+        string output = "Receive channels: " + string.Join(", ", foundChannels);
+        return output;
+    }
+}
